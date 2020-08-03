ace69c6bd1922c8dee02c3c995f690e4844c1663
Ben Martin <ben_m365@hotmail.co.uk>
2020-08-04 00:55:53 +0100

    change command syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ do
   EXCLUDE="$EXCLUDE --exclude $excludedPath"
 done
 
-lftp open $INPUT_FTP_HOST -p $INPUT_FTP_PORT -u $INPUT_FTP_USERNAME,$INPUT_FTP_PASSWORD -e "$([ "$INPUT_DISABLE_SSL_CERTIFICATE_VERIFICATION" == true ] && echo "set ssl:verify-certificate false;") mirror --verbose --reverse $([ "$INPUT_DELETE" == true ] && echo "--delete-first") $EXCLUDE $INPUT_LOCAL_SOURCE_DIR $INPUT_DIST_TARGET_DIR; exit"
+lftp -u $INPUT_FTP_USERNAME,$INPUT_FTP_PASSWORD -p $INPUT_FTP_PORT -e "$([ "$INPUT_DISABLE_SSL_CERTIFICATE_VERIFICATION" == true ] && echo "set ssl:verify-certificate false;") mirror --verbose --reverse $([ "$INPUT_DELETE" == true ] && echo "--delete-first") $INPUT_LOCAL_SOURCE_DIR $INPUT_DIST_TARGET_DIR; exit" $INPUT_FTP_HOST
